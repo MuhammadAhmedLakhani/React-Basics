422005ff2045954c1e636a5f5dff18c354762d8e
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-20 17:15:59 +0500

    React:Variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import img from './assets/images/image.jpeg'
 
+let name = "Muhammad Ahmed";
+
 function App() {
   return (
     <div>
-      <h1 style={{backgroundColor:"grey",border:"2px solid #000"}} >Hello </h1>
+      <h1 style={{backgroundColor:"grey",border:"2px solid #000"}} >{name}</h1>
       <img src={img} alt="" />
     </div>
   );
