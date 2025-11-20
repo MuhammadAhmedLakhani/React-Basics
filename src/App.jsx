7e1426a41c0c738c1cc73d05e6b19b7cea0f2796
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-20 16:34:32 +0500

    React:CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{backgroundColor:"grey",border:"2px solid #000"}} >Hello </h1>
+    </div>
+  );
+}
+
+export default App;
