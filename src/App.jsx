6ac2259fa97aaccd44599eb0ec72c71e65f4ade2
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-20 17:40:51 +0500

    React:Function (argument)

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import img from './assets/images/image.jpeg'
 
 function App() {
   let name = "Muhammad Ahmed";
-  const foo = ()=>{
-    console.log("running..")
+  const foo = (a)=>{
+    console.log("running..",a)
   }
   return (
     <div>
       <h1 style={{backgroundColor:"grey",border:"2px solid #000"}} >{name}</h1>
-      <button onClick={()=>foo()}>Click Me</button>
+      <button onClick={()=>foo("Hello")}>Click Me</button>
       <br />
       <img src={img} alt="" />
     </div>
