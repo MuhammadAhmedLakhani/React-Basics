f2e0d8dab9294ed7f654392a41bdbd577daf6a8f
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-20 19:36:34 +0500

    React:component composition

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 import img from './assets/images/image.jpeg'
 
-let students = [ {
-  name:"Ahmed",
-  email:"Ahmed@gmail.com"
-},{
-  name:"Ghous",
-  email:"Ghous@gmail.com"
-}]
+
+
+// function App() {
+//   let name = "Muhammad Ahmed";
+//   const foo = (a)=>{
+//     console.log("running..",a)
+//   }
+//   return (
+//     <div>
+//       <h1 style={{backgroundColor:"grey",border:"2px solid #000"}} >{name}</h1>
+//       <ul>
+//       {
+//        students.map((elem , i)=> <li>{elem.name}</li>)
+//        }
+
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+function Header(){
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+function Footer(){
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  )
+}
 
 
 function App() {
-  let name = "Muhammad Ahmed";
-  const foo = (a)=>{
-    console.log("running..",a)
-  }
   return (
     <div>
-      <h1 style={{backgroundColor:"grey",border:"2px solid #000"}} >{name}</h1>
-      <ul>
-      {
-       students.map((elem , i)=> <li>{elem.name}</li>)
-       }
-
-      </ul>
+      <Header/>
+      <div>
+        <p>SMIT</p>
+      </div>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
