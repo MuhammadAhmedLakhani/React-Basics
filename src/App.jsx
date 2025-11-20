92e21e76b534ea8670bc80f1f175259f9f291573
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-20 17:55:07 +0500

    React:Object

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import img from './assets/images/image.jpeg'
 
+let obj = {
+  name:"Ahmed",
+  emial:"Ahmed@gmail.com"
+}
+
 
 function App() {
   let name = "Muhammad Ahmed";
@@ -11,6 +16,7 @@ function App() {
   return (
     <div>
       <h1 style={{backgroundColor:"grey",border:"2px solid #000"}} >{name}</h1>
+      <h2>{obj.name}</h2>
       <button onClick={()=>foo("Hello")}>Click Me</button>
       <br />
       <img src={img} alt="" />
