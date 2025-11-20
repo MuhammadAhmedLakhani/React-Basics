cf0fedabc95d2d15806476cbf6c13a2eda0895ae
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-20 20:30:13 +0500

    React:component composition V3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import {Header , Footer} from './components/component'
 
 
 // function App() {
